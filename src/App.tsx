a4e003b7a605ce07132be4da6da48501da3c72f6
Amin <aminparvaresh2014@gmail.com>
2026-08-18 10:05:44 +0100

    chore: fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import { getPokemon } from "./api/pokemonApi";
 
 function App() {
-
   useEffect(() => {
-    getPokemon("pikachu").then((pokemon) => { console.log(pokemon); });
+    getPokemon("pikachu").then((pokemon) => {
+      console.log(pokemon);
+    });
   }, []);
 
   return <h1>Pokémon App</h1>;
