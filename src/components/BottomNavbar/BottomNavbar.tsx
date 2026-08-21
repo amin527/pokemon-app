13a3323148c3f10bc519695766fdccd2b8b3c333
Amin <aminparvaresh2014@gmail.com>
2026-08-21 14:45:10 +0100

    feat/bottom-navbar

---
+++ src/components/BottomNavbar/BottomNavbar.tsx
@@ -0,0 +1,14 @@
+import "./BottomNavbar.css"
+
+type BottomNavbarProps = {
+  children: React.ReactNode;
+};
+
+function BottomNavbar({ children }: BottomNavbarProps) {
+  return (
+    <div className="bottom-nav-bar">
+      {children}
+    </div>
+  );
+}
+export default BottomNavbar;
