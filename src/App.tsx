7b9a1d21a6923770444b293446267f6ee815a99b
Amin <aminparvaresh2014@gmail.com>
2026-08-26 10:00:43 +0100

    Selected theme now persists between user sessions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import LandingDisplay from "./components/LandingDisplay/LandingDisplay";
 
 type ThemeContextType = { 
   theme: string;
-  setTheme: ((value: string) => void) ;
+  setTheme: (value: string) => void;
 }
 
 export const ThemeContext = createContext<ThemeContextType>({theme: "", setTheme: () =>{}})
@@ -12,6 +12,12 @@ export const ThemeContext = createContext<ThemeContextType>({theme: "", setTheme
 function App() {
 
   const [theme, setTheme] = useState<string>("light")
+
+  useEffect(() => {
+    const loadedTheme = localStorage.getItem("theme")
+    if (loadedTheme) { setTheme(loadedTheme) }
+  }, [])
+
   return (
     <div data-testid="app-component-content">
       <ThemeContext.Provider value={{ theme, setTheme }}>
@@ -22,3 +28,7 @@ function App() {
 }
 
 export default App;
+function useEffectLayout(arg0: () => void) {
+  throw new Error("Function not implemented.");
+}
+
--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -9,16 +9,26 @@ type DropDownProps = {
 
 function DropDown({ setSettingsDropDownIsVisible }: DropDownProps) {
 
+  
   const { theme, setTheme } = useContext(ThemeContext) 
   const DropDownClassName = theme == "light" ? "drop-down" : "drop-down dark" 
-
+  function applyDarkTheme() {
+    setTheme("dark"); 
+    setSettingsDropDownIsVisible(false);
+    localStorage.setItem("theme", "dark")
+  }
+  function applyLightTheme() {
+    setTheme("light"); 
+    setSettingsDropDownIsVisible(false);
+    localStorage.setItem("theme", "light")
+  }
   return (
     <div className={DropDownClassName} data-testid="drop-down-component">
       <div className="theme-options-container">
         <div className="">THEME OPTIONS</div>
         <div className="theme-options">
-            <Button text="Dark" handleClick={() => {setTheme("dark"); setSettingsDropDownIsVisible(false);}} />  
-            <Button text="Light" handleClick={() => {setTheme("light"); setSettingsDropDownIsVisible(false); }} />
+            <Button text="Dark" handleClick={applyDarkTheme} />  
+            <Button text="Light" handleClick={applyLightTheme} />
         </div>
       </div>
     </div>
