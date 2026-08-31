786726cf8a5343076c12c9194db813496dbe247c
Amin <aminparvaresh2014@gmail.com>
2026-08-31 11:45:31 +0100

    chore: resolved lint error

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import "./PokemonCard.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { Link } from "react-router";
@@ -23,33 +23,32 @@ function PokemonCard({ id, name, image, types }: Pokemon) {
   }
 
   return (
-    <Link 
-      to={`/pokemon/${id}`} 
+    <Link
+      to={`/pokemon/${id}`}
       onClick={handleClick}
-      className="pokemon-card-link"  
+      className="pokemon-card-link"
     >
       <div
         className={`pokemon-card ${theme == "light" ? "" : "pokemon-card--dark"}`}
         data-testid="pokemon-card"
       >
         <img className="pokemon-card__artwork" src={image} alt={name} />
-        <div 
-          data-testid="pokemon-card-display" 
+        <div
+          data-testid="pokemon-card-display"
           className="pokemon-card-display"
         >
-          <div className="pokemon-card-display__name">{name.charAt(0).toUpperCase() + name.slice(1)}</div>
+          <div className="pokemon-card-display__name">
+            {name.charAt(0).toUpperCase() + name.slice(1)}
+          </div>
           <div className="pokemon-card-display__id">#{id}</div>
         </div>
-        <div
-          className="pokemon-card__info"
-          data-testid="pokemon-card-info"
-        >
+        <div className="pokemon-card__info" data-testid="pokemon-card-info">
           <div className="pokemon-card__name-and-id">
             <div className="pokemon-card-info__name">
-              <strong>Name: </strong>{name.charAt(0).toUpperCase() + name.slice(1)}
+              <strong>Name: </strong>
+              {name.charAt(0).toUpperCase() + name.slice(1)}
             </div>
-            <div className="pokemon-card-info__id">
-              ID: {id}</div>
+            <div className="pokemon-card-info__id">ID: {id}</div>
           </div>
           <div className="pokemon-card-info__types">
             <strong>Types:</strong>{" "}
